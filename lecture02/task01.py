389e74f0083e3a768d40e3436ed25bf017b5681a
jwkuhr99 <kuhrtje@hotmail.com>
2025-09-02 11:15:48 +0300

    I have finished task01 from lecture02

--- lecture02/task01.py
+++ lecture02/task01.py
@@ -1,1 +1,3 @@
-print("Justin")
+celsius = int(input("What is the temperature in Celsius? I will calculate it to Fahrenheit : "))
+celsiusToFahrenheit = (celsius * 9 / 5) + 32
+print(f"That's approximately {celsiusToFahrenheit:2.0f} degrees in Fahrenheit.")
