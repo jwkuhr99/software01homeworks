4f4ecf4ef4cf2dd6d83c4fd622d8118bee35eb23
jwkuhr99 <kuhrtje@hotmail.com>
2025-09-08 20:11:34 +0300

    I have finished task03 from lecture02

--- lecture02/task03.py
+++ lecture02/task03.py
@@ -1,8 +1,11 @@
+#vraag de gebruiker naar de lengte en breedte van de rechthoek
 length = float(input("Enter the length of the rectangle: "))
 width = float(input("Enter the width of the rectangle: "))
 
+#bereken de oppervlakte en omtrek
 area = length * width
 perimeter = 2 * (length + width)
 
+#laat de berekeningen zien
 print(f"The area of the rectangle is: {area:.2f}.")
 print(f"The perimeter of the rectangle is: {perimeter:.2f}.")
