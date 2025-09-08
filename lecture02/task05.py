90e4d67f918c4290be6bd8bf10b79af699446881
jwkuhr99 <kuhrtje@hotmail.com>
2025-09-08 20:16:38 +0300

    I have finished task05 from lecture02

--- lecture02/task05.py
+++ lecture02/task05.py
@@ -1,16 +1,22 @@
+#vraag de gebruiker om talents, pounds en lots
 talents = int(input("Enter the number of talents (leiviskä): "))
 pounds = int(input("Enter the number of pounds (naula): "))
 lots = int(input("Enter the number of lots (luoti): "))
 
+#conversions
 pounds_in_talents = 20
 lots_in_pounds = 32
 grams_in_lots = 13.3
 
+#bereken het totaal in lots
 total_lots = (talents * pounds_in_talents * lots_in_pounds) + (pounds * lots_in_pounds) + lots
 
+#bereken het totaal in gram
 total_grams = total_lots * grams_in_lots
 
+#bereken de resultaten om in kilogram en de rest in gram
 kilograms = int(total_grams / 1000)
 grams = round(total_grams % 1000 , 2)
 
+#laat de resultaten zien
 print(f"The weight in modern units is {kilograms} kilograms and {grams} grams")
