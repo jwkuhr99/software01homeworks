f6bf83adde5209c6f404b4349ce47e2a666f829a
jwkuhr99 <kuhrtje@hotmail.com>
2025-09-08 20:09:55 +0300

    I have finished task02 from lecture02

--- lecture02/task02.py
+++ lecture02/task02.py
@@ -1,7 +1,10 @@
 import math
 
+#vraag de gebruiker naar de radius
 radius = float(input("Enter the radius of the circle: "))
 
+#bereken de oppervlakte
 area = math.pi * radius ** 2
 
+#laat de oppervlakte zien afgerond op 2 decimalen
 print(f"The area of the circle with radius {radius:.2f} is {area:.2f}")
