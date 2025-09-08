82553a10592d7872cce0fa940153161589c20443
jwkuhr99 <kuhrtje@hotmail.com>
2025-09-08 19:38:49 +0300

    I have finished task04 from lecture02

---
+++ lecture02/task04.py
@@ -0,0 +1,9 @@
+num1 = int(input("Give me the first number: "))
+num2 = int(input("Give me the second number: "))
+num3 = int(input("Give me the third number: "))
+total = num1 + num2 + num3
+product = num1 * num2 * num3
+average = total/3
+print(f"The total of the numbers is: {total:.0f}.")
+print(f"The product of the numbers is: {product:.0f}.")
+print(f"The average of the numbers is: {average:.0f}.")
