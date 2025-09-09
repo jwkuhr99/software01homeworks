6641b44f0871f137d1cc2a37a1c8d42c483b9a49
jwkuhr99 <kuhrtje@hotmail.com>
2025-09-09 11:43:37 +0300

    I have completed task03 for lecture03

---
+++ lecture03/task03.py
@@ -0,0 +1,25 @@
+gender = input("Are you a male or female? ")
+
+
+if gender.lower() == "male":
+    hemoglobin_level = float(input("Enter your hemoglobin level (g/L): "))
+
+    if hemoglobin_level < 134:
+        print("\nHemoglobin level too low.")
+    elif hemoglobin_level >= 134 and hemoglobin_level <= 167:
+        print("\nHemoglobin level normal.")
+    elif hemoglobin_level > 167:
+        print("\nHemoglobin level too high.")
+
+elif gender.lower() == "female":
+    hemoglobin_level = float(input("Enter your hemoglobin level (g/L): "))
+
+    if hemoglobin_level < 117:
+        print("\nHemoglobin level too low.")
+    elif hemoglobin_level >= 117 and hemoglobin_level <= 155:
+        print("\nHemoglobin level normal.")
+    elif hemoglobin_level > 155:
+        print("\nHemoglobin level too high.")
+
+else:
+    print("\nPlease enter a valid gender.")
