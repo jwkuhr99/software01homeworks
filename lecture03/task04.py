a5f2b76bf4ffcb159172637495107edb6092b79b
jwkuhr99 <kuhrtje@hotmail.com>
2025-09-16 10:29:48 +0300

    I have completed task04 for lecture03

--- lecture03/task04.py
+++ lecture03/task04.py
@@ -1,12 +1,6 @@
 year = int(input("Enter a year, to see if it's a leap year: "))
 
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-            print(f"The year {year} is a leap year.")
-        else:
-            print(f"The year {year} is not a leap year.")
-    else:
-        print(f"The year {year} is not a leap year.")
+if year % 4 == 0 or (year % 100 == 0 and year % 400 == 0):
+    print(f"The year {year} is a leap year.")
 else:
     print(f"The year {year} is not a leap year.")
