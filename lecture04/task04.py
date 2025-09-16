ff7f55b6dda1fcde5ea17e8ba65df711737616f0
jwkuhr99 <kuhrtje@hotmail.com>
2025-09-16 10:20:54 +0300

    I have completed task04 for lecture04

---
+++ lecture04/task04.py
@@ -0,0 +1,23 @@
+import random
+
+def guess_the_number():
+    number_to_guess = random.randint(1,10)
+    print("I'm thinking of a number between 1 and 10. Can you guess it?")
+
+    while True:
+        try:
+            user_guess = int(input("Your guess: "))
+            if user_guess <1 or user_guess > 10:
+                print("\nPlease enter a number between 1 and 10.")
+                continue
+            if user_guess < number_to_guess:
+                print("\nHigher!")
+            elif user_guess > number_to_guess:
+                print("\nLower!")
+            else:
+                print("\nCorrect! That was the number. :))")
+                break
+        except ValueError:
+            print("\nThat's not a valid number. Please try an integer number between 1 and 10.")
+
+guess_the_number()
