3ed5b9ac2054eb3bdce0a0975f9dc83d22cd5944
jwkuhr99 <kuhrtje@hotmail.com>
2025-09-23 11:42:06 +0300

    I have completed task04 for lecture04

--- lecture04/task04.py
+++ lecture04/task04.py
@@ -5,19 +5,16 @@ def guess_the_number():
     print("I'm thinking of a number between 1 and 10. Can you guess it?")
 
     while True:
-        try:
-            user_guess = int(input("Your guess: "))
-            if user_guess <1 or user_guess > 10:
-                print("\nPlease enter a number between 1 and 10.")
-                continue
-            if user_guess < number_to_guess:
-                print("\nHigher!")
-            elif user_guess > number_to_guess:
-                print("\nLower!")
-            else:
-                print("\nCorrect! That was the number. :))")
-                break
-        except ValueError:
-            print("\nThat's not a valid number. Please try an integer number between 1 and 10.")
+        user_guess = int(input("Your guess: "))
+        if user_guess <1 or user_guess > 10:
+            print("\nPlease enter a number between 1 and 10.")
+            continue
+        if user_guess < number_to_guess:
+            print("\nHigher!")
+        elif user_guess > number_to_guess:
+            print("\nLower!")
+        else:
+            print("\nCorrect! That was the number. :))")
+            break
 
 guess_the_number()
